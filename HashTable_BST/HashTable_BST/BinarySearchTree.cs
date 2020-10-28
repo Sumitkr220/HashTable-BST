de8a5343d90c15802af5b45ac74fcd94e8c8ed7f
sumitpandit <sumitpandit178@gmail.com>
2020-10-28 13:28:33 +0530

    [Sumit] Add: Create add method and check size of BST

--- HashTable_BST/HashTable_BST/BinarySearchTree.cs
+++ HashTable_BST/HashTable_BST/BinarySearchTree.cs
@@ -21,6 +21,14 @@ namespace HashTable_BST
                 treeSize++;
             }
         }
+        public int Size()
+        {
+            if (root == null)
+            {
+                Console.WriteLine("Binary Search Tree is Empty");
+            }
+            return treeSize;
+        }
         public void Display()
         {
             if (root == null)
